2d349949cb0d2bc23dd4f47f9c2745616f3edf1d
minkatan <minka.tan@gmail.com>
2022-08-27 21:59:37 -0400

    qrcode-page

---
+++ qr-code-component-main/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      cwhite: 'hsl(0, 0%, 100%)',
+      lightgray: 'hsl(212, 45%, 89%)',
+      grayblue: 'hsl(220, 15%, 55%)',
+      darkblue: 'hsl(218, 44%, 22%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
